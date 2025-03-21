b2827ed3832338343610ef95cd678c8c3f784ad8
Donggeun Ko <72428646+seanko29@users.noreply.github.com>
2025-03-22 03:07:04 +0900

    Delete run_valid.sh

--- run_valid.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=0 python test_demo.py \
-    --data_dir /home/sean/ntire_data/real_validation \
-    --save_dir /home/sean/SR_project/ACCV/NTIRE2025_ESR/results \
-    --model_id 11       
